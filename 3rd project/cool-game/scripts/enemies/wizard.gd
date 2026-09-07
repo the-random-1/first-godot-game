57faab7e6c594b04a00bf0ea8a6e42a543d0dc87
the-random-1 <rrajeshlolxd@gmail.com>
2026-09-06 23:50:19 -0500

    i forgot

--- 3rd project/cool-game/scripts/enemies/wizard.gd
+++ 3rd project/cool-game/scripts/enemies/wizard.gd
@@ -1,2 +1,21 @@
 extends Enemy
 class_name Wizard
+
+func changestate(newstate: _STATES) -> void:
+	state = newstate
+	match newstate:
+		_STATES.IDLE:
+			forces[0] = Vector2.ZERO
+			$AnimatedSprite2D.play("idle")
+		_STATES.WALK:
+			$AnimatedSprite2D.play("walk")
+
+func _enemyinit() -> void:
+	speed = 75.0
+	max_health = 50.0
+	health = max_health
+	damage = 25.0
+	kb = 2.2
+	wander_time = Vector2(1.0, 2.0)
+	m = 1.0
+	rand = increaseMagnitude(rand)
